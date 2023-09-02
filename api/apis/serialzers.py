6278eb36464074c2372e0a5f607aa0b298c6ab86
godeytech <godfredtenkorang9@gmail.com>
2023-09-02 14:43:50 +0000

    changes

--- api/apis/serialzers.py
+++ api/apis/serialzers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
 from api.models import Blog
 
-class BlogSerializer(serializers.HyperlinkedModelSerializer):
+class BlogSerializer(serializers.ModelSerializer):
     
-    username = serializers.SerializerMethodField('get_username_from_author')
+    # username = serializers.SerializerMethodField('get_username_from_author')
     
     class Meta:
         model = Blog
-        fields = ['url', 'title', 'content', 'image', 'date_posted', 'username']
+        fields = ['url', 'title', 'content', 'image', 'date_posted', 'author',]
         
-    def get_username_from_author(self, blog):
-        username = blog.author.username
-        return username
+    # def get_username_from_author(self, blog):
+    #     username = blog.author.username
+    #     return username
