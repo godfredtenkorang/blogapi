07a205f10dd8f8555fd560452c4d7452f018250f
godeytech <godfredtenkorang9@gmail.com>
2023-09-16 12:37:31 +0000

    changes

--- users/apis/serializers.py
+++ users/apis/serializers.py
@@ -5,29 +5,29 @@ from users.models import Profile
 
 class RegistrationSerializer(serializers.ModelSerializer):
     
-    # password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
     
     class Meta:
         model = User
         fields = ['email', 'username', 'password']
-        # extra_kwargs = {
-        #     'password': {'write_only':True}
-        # }
+        extra_kwargs = {
+            'password': {'write_only':True}
+        }
         
-    # def save(self):
-    #     user = User (
-    #         email=self.validated_data['email'],
-    #         username=self.validated_data['username']
-    #     )
+    def save(self):
+        user = User (
+            email=self.validated_data['email'],
+            username=self.validated_data['username']
+        )
         
-    #     password = self.validated_data['password']
-    #     password2 = self.validated_data['password2']
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
         
-    #     if password != password2:
-    #         raise serializers.ValidationError({'password': 'Passwords must match'})
-    #     user.set_password(password)
-    #     user.save()
-    #     return user
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match'})
+        user.set_password(password)
+        user.save()
+        return user
     
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
